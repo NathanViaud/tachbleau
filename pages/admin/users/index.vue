badeb433bc6e109f0850b1979516accb6138a36e
amine-asfar <amine.asfar294@gmail.com>
2024-05-13 16:19:03 +0200

    Page Admin/users

---
+++ pages/admin/users/index.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { Trash2,Pencil } from 'lucide-vue-next';
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table'
+import { Button } from '@/components/ui/button'
+
+</script>
+
+<template>
+        <Table>
+            <TableCaption>A list of your recent invoices.</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead class="w-[100px] text-center">
+                        Email
+                    </TableHead>
+                    <TableHead class="text-center">Rôle</TableHead>
+<!--                    <TableHead class="text-right">Edit</TableHead>-->
+                    <TableHead class="text-center">
+                        Actions
+                    </TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                <TableRow>
+                    <TableCell class="font-medium text-center">
+                        Admin@test.fr
+                    </TableCell>
+                    <TableCell class="text-center">Admin</TableCell>
+                    <TableCell class="text-center">
+                        <div>
+                            <Button variant="destructive" class="m-1"> <Trash2/></Button>
+                            <Button><Pencil color="white" class="m-1" /></Button>
+                        </div>
+                    </TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+</template>
+
