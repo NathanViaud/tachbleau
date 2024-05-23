6e2ed8fdc059162ac9b6cbcd10e68046c88e6011
VIAUD Nathan <nathan.viaud@inria.fr>
2024-05-23 14:58:19 +0200

    feat: Project display advancement in linked tasks

--- components/projects/Item.vue
+++ components/projects/Item.vue
@@ -1,27 +1,37 @@
 <script setup lang="ts">
 import type { Project } from '~/types';
 import { Progress } from '~/components/ui/progress';
+import { useTasks } from '~/stores/tasks.store';
+import { useProjects } from '~/stores/projects.store';
 
-defineProps<{
+const props = defineProps<{
     project: Project;
 }>();
 
-const progress = ref(20);
+const projectsStore = useProjects();
+const tasksStore = useTasks();
+const projectAdvancement = computed(() => projectsStore.getProjectAdvancement(props.project._id));
+
+// TODO: This should be done in a middleware
+onMounted(() => {
+    if(!tasksStore.tasks.length) tasksStore.fetchTasks();
+    if(!projectsStore.projects.length) projectsStore.fetchProjects();
+})
 
 </script>
 
 <template>
-    <div class="border rounded border-muted p-4 flex gap-2 items-center">
+    <div class="border rounded border-muted p-4 flex gap-2 items-center" v-if="tasksStore.tasks.length">
         <div class="flex flex-col flex-1">
             <h2 class="text-lg font-semibold">{{ project.title }}</h2>
             <span class="text-muted-foreground">{{ formatDate(project.startDate) }} - {{ formatDate(project.endDate) }}</span>
         </div>
 
         <div class="flex flex-col max-w-xl w-full gap-2">
-            <Progress v-model="progress" class="h-2.5" />
+            <Progress :model-value="projectAdvancement.percentage" class="h-2.5" />
             <div class="flex gap-2 justify-between">
-                <span>{{ progress }}%</span>
-                <span>0/12 tasks</span>
+                <span>{{ projectAdvancement.percentage }}%</span>
+                <span>{{ projectAdvancement.done }} / {{ projectAdvancement.tasks }} tasks</span>
             </div>
         </div>
     </div>
---
+++ stores/projects.store.ts
@@ -0,0 +1,46 @@
+import { defineStore } from 'pinia';
+import type { Project, Task } from '~/types';
+import { getProjects } from '~/services';
+
+interface ProjectsState {
+    projects: Project[];
+}
+
+export const useProjects = defineStore('projects', {
+    state: (): ProjectsState => ({
+        projects: []
+    }),
+    
+    actions: {
+        async fetchProjects() {
+            this.projects = await getProjects();
+        },
+        
+        getProjectTasks(projectId: string) {
+            const tasksStore = useTasks();
+            
+            return tasksStore.tasks.filter((task) => task.project === projectId);
+        },
+        
+        getProjectAdvancement(projectId: string) {
+            const res = {
+                percentage: 0,
+                tasks: 0,
+                done: 0
+            }
+            
+            const projectTasks = this.getProjectTasks(projectId);
+            if(!projectTasks.length) return res;
+            
+            const doneTasks = projectTasks.filter((task: Task) => task.status === 'done');
+            const totalTasks = projectTasks.length;
+            const percentage = Math.round((doneTasks.length / totalTasks) * 100);
+            
+            return {
+                percentage,
+                tasks: totalTasks,
+                done: doneTasks.length
+            }
+        }
+    }
+})
