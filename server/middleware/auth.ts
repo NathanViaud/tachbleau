10a9d54bf3e2b3c96f86d62913b12c388dff8443
VIAUD Nathan <nathan.viaud@inria.fr>
2024-06-19 10:09:57 +0200

    fix: server middleware

---
+++ server/middleware/auth.ts
@@ -0,0 +1,56 @@
+import { verifyToken } from '~/server/utils/session';
+import type { JwtPayload } from 'jsonwebtoken';
+
+export default defineEventHandler(async (event) => {
+    const adminRoutes = [
+        { url: '/api/users/:id', method: 'DELETE' },
+        { url: '/api/users/:id', method: 'PUT' },
+        { url: '/api/users', method: 'POST' },
+    ]
+    const userRoutes = [
+        // Projects
+        { url: '/api/projects/:id', method: 'DELETE' },
+        { url: '/api/projects/:id', method: 'GET' },
+        { url: '/api/projects/:id', method: 'PUT' },
+        { url: '/api/projects', method: 'POST' },
+        { url: '/api/projects', method: 'GET' },
+        
+        // Tasks
+        { url: '/api/tasks/:id', method: 'DELETE' },
+        { url: '/api/tasks/:id', method: 'GET' },
+        { url: '/api/tasks/:id', method: 'PUT' },
+        { url: '/api/tasks', method: 'POST' },
+        { url: '/api/tasks', method: 'GET' },
+        
+        // Users
+        { url: '/api/users', method: 'GET' },
+        { url: '/api/users/:id', method: 'GET' },
+        { url: '/api/users/logout', method: 'POST' },
+        { url: '/api/users/update', method: 'PUT' }
+    ];
+    
+    const guestRoutes = [
+        { url: '/api/users/login', method: 'POST' },
+        { url: '/api/users/token', method: 'POST' },
+        { url: '/api/users/token/verify', method: 'POST' },
+    ]
+    
+    if (event.path.startsWith('/api')) {
+        const isGuestRoute = guestRoutes.find(route => event.path.startsWith(route.url) && event.method === route.method);
+        if (isGuestRoute) return;
+        
+        const token = getCookie(event, 'token');
+        if (!token) return { status: 401, body: 'Unauthorized user no token' };
+        const verifiedToken = await verifyToken(token) as JwtPayload;
+        if (!verifiedToken) return { status: 401, body: 'Unauthorized user no user' };
+        
+        const isUserRoute = userRoutes.find(route => event.path.startsWith(route.url) && event.method === route.method);
+        if (isUserRoute) return;
+        
+        const isAdminRoute = adminRoutes.find(route => event.path.startsWith(route.url) && event.method === route.method);
+        if (isAdminRoute) {
+            if (verifiedToken.role === 'admin') return;
+            return { status: 403, body: 'Unauthorized user not admin' };
+        }
+    }
+})
