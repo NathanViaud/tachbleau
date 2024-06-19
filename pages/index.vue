1023f1c2e45e4b5932b9d7a73faef273eded7aa7
Julien-Clerc <julien.clerc@epitech.eu>
2024-06-19 14:58:19 +0200

    🐛 remove index card to userAdmin

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,8 @@
 <script lang="ts" setup>
+import { useUsers } from '~/stores/users.store';
+
+const usersStore = useUsers();
+
 definePageMeta({
     middleware: 'user-only'
 })
@@ -6,24 +10,22 @@ definePageMeta({
 
 <template>
     <div class="mx-12">
-        <div class="grid grid-cols-3 gap-4 flex-col">
-            <div class="flex w-full justify-between col-span-2 px-12 gap-4">
-                <indexCard 
-                    title="Gestion des tâches / KANBAN" 
-                    description="Création, modification et suppression des tâches. Assignation des tâches"
-                    toRoute="/kanban"
-                />
-                <indexCard 
-                    title="Gestion des Projets" 
-                    description="Création, modification et suppression des projets"
-                    toRoute="/projects"
-                />
-                <indexCard 
-                    title="Gestion des membres de l'équipe" 
-                    description="Création, modification et suppression d'un membre de l'équipe"
-                    toRoute="/stafflist"
-                />   
-            </div>
+        <div class="flex w-full col-span-2 px-12 gap-8">
+            <indexCard 
+                title="Gestion des tâches / KANBAN" 
+                description="Création, modification et suppression des tâches. Assignation des tâches"
+                toRoute="/kanban"
+            />
+            <indexCard 
+                title="Gestion des Projets" 
+                description="Création, modification et suppression des projets"
+                toRoute="/projects"
+            />
+            <indexCard v-if="usersStore.isAdmin"
+                title="Gestion des membres de l'équipe" 
+                description="Création, modification et suppression d'un membre de l'équipe"
+                toRoute="/admin/users"
+            />   
         </div>
         <div class="mt-20 px-8">
             <div class="my-8">
