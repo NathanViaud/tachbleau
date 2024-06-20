c903e8b49ebfafa0bcacea414b6ba55021117827
Julien-Clerc <julien.clerc@epitech.eu>
2024-06-20 10:42:18 +0200

    💄 add card hover

--- components/index/Card.vue
+++ components/index/Card.vue
@@ -23,12 +23,12 @@ defineProps({
 </script>
 
 <template>
-  <router-link :to="toRoute">
-    <Card class="w-72 h-40 cursor-pointer border-2">
+  <Card class="w-72 h-40 cursor-pointer border-2 hover:bg-card-foreground/5" as-child>
+    <nuxt-link :to="toRoute">
       <CardHeader>
-        <CardTitle>{{ title }}</CardTitle>
-        <CardDescription>{{ description }}</CardDescription>
-      </CardHeader>
-    </Card>
-  </router-link>
+      <CardTitle>{{ title }}</CardTitle>
+      <CardDescription>{{ description }}</CardDescription>
+    </CardHeader>
+    </nuxt-link>
+  </Card>
 </template>
