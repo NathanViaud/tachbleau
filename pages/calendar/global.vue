877ea8d3165d27615c0878952b4af8eb802f3038
Julien-Clerc <julien.clerc@epitech.eu>
2024-07-17 16:40:19 +0200

    test8

--- pages/calendar/global.vue
+++ pages/calendar/global.vue
@@ -1,31 +1,21 @@
-<script>
+<script setup lang="ts">
 import FullCalendar from '@fullcalendar/vue3'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from '@fullcalendar/interaction'
 
-export default {
-  components: {
-    FullCalendar // make the <FullCalendar> tag available
-  },
-  data() {
-    return {
-      calendarOptions: {
-        plugins: [ dayGridPlugin, interactionPlugin ],
-        initialView: 'dayGridMonth',
-        dateClick: this.handleDateClick,
-        events: [
-          { title: 'event 1', date: '2019-04-01' },
-          { title: 'event 2', date: '2019-04-02' }
-        ]
-      }
-    }
-  },
-  methods: {
-    handleDateClick: function(arg) {
-      alert('date click! ' + arg.dateStr)
-    }
-  }
-}
+const calendarOptions = {
+  plugins: [ dayGridPlugin, interactionPlugin ],
+  initialView: 'dayGridMonth',
+  dateClick: handleDateClick,
+  events: [
+    { title: 'event 1', date: '2019-04-01' },
+    { title: 'event 2', date: '2019-04-02' }
+  ]
+};
+
+function handleDateClick(arg) {
+  alert('date click! ' + arg.dateStr)
+};
 
 definePageMeta({
     middleware: 'user-only'
