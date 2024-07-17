924c9952a710239776bd5936dd408b29fb64c016
Julien-Clerc <julien.clerc@epitech.eu>
2024-07-17 15:39:53 +0200

    test6

--- pages/calendar/global.vue
+++ pages/calendar/global.vue
@@ -1,33 +1,33 @@
 <script>
-// import FullCalendar from '@fullcalendar/vue3'
-// import dayGridPlugin from '@fullcalendar/daygrid'
-// // const dayGridPlugin = (await import('@fullcalendar/daygrid')).default;
-// import interactionPlugin from '@fullcalendar/interaction'
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+// const dayGridPlugin = (await import('@fullcalendar/daygrid')).default;
+import interactionPlugin from '@fullcalendar/interaction'
 
-// export default {
-//   components: {
-//     FullCalendar // make the <FullCalendar> tag available
-//   },
-//   data() {
-//     return {
-//       calendarOptions: {
-//         plugins: [ dayGridPlugin, interactionPlugin ],
-//         initialView: 'dayGridMonth',
-//         dateClick: this.handleDateClick,
-//         events: [
-//           { title: 'event 1', date: '2019-04-01' },
-//           { title: 'event 2', date: '2019-04-02' }
-//         ]
-//       }
-//     }
-//   },
-//   methods: {
-//     handleDateClick: function(arg) {
-//       alert('date click! ' + arg.dateStr)
-//     }
-//   }
-// }
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth',
+        dateClick: this.handleDateClick,
+        events: [
+          { title: 'event 1', date: '2019-04-01' },
+          { title: 'event 2', date: '2019-04-02' }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleDateClick: function(arg) {
+      alert('date click! ' + arg.dateStr)
+    }
+  }
+}
 </script>
 <template>
-  <h1>Calendar</h1>
+  <FullCalendar :options="calendarOptions" />
 </template>
