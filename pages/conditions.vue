943774a5343495b0a360c86e1cd5b39655ac2f10
Oussama Fatnassi <oussamafatnassi@icloud.com>
2024-07-30 11:29:53 +0200

    added general conditions page

---
+++ pages/conditions.vue
@@ -0,0 +1,121 @@
+
+<template>
+  <div class="min-h-screen bg-gray-100 py-10">
+    <div class="container mx-auto bg-white p-6 rounded-lg shadow-md">
+      <h1 class="text-center text-3xl font-bold mb-8">Conditions générales d'utilisation</h1>
+      <div class="flex flex-col gap-6">
+        <section class="text-center text-sm">
+          <h2 class="text-2xl font-semibold mb-4">Préambule</h2>
+          <p>
+            Les présentes Conditions Générales d'Utilisation (CGU) régissent l'utilisation de la plateforme collaborative en ligne destinée aux étudiants pour la gestion de projets et la création de mind maps (ci-après dénommée "la Plateforme"). En accédant et en utilisant la Plateforme, vous acceptez sans réserve les présentes CGU.
+          </p>
+        </section>
+        
+        <section class="text-center text-sm">
+          <h2 class="text-2xl font-semibold mb-4">1. Définitions</h2>
+          <p>Utilisateur : Toute personne physique ou morale utilisant la Plateforme.</p>
+          <p>Compte : Espace personnel de l'Utilisateur sur la Plateforme.</p>
+          <p>Données Personnelles : Toute information relative à une personne physique identifiée ou identifiable.</p>
+          <p>Contenu : Toute information, donnée, texte, image, vidéo, ou autre contenu publié sur la Plateforme par les Utilisateurs.</p>
+        </section>
+        
+        <section class="text-center text-sm">
+          <h2 class="text-2xl font-semibold mb-4">2. Accès et Inscription</h2>
+          <h3 class="text-xl font-semibold mb-2">2.1. Accès à la Plateforme</h3>
+          <p>L'accès à la Plateforme est réservé aux étudiants disposant d'une adresse email en '@myges.fr'.</p>
+          
+          <h3 class="text-xl font-semibold mt-4 mb-2">2.2. Inscription</h3>
+          <p>Pour utiliser la Plateforme, l'Utilisateur doit créer un Compte en fournissant des informations personnelles exactes et complètes. L'Utilisateur s'engage à maintenir ces informations à jour.</p>
+          
+          <h3 class="text-xl font-semibold mt-4 mb-2">2.3. Sécurité des Comptes</h3>
+          <p>L'Utilisateur est responsable de la sécurité de son Compte et de son mot de passe. Il s'engage à ne pas divulguer ses identifiants de connexion et à informer immédiatement l'administrateur de la Plateforme en cas de perte ou de vol de ses identifiants.</p>
+        </section>
+
+        <section class="text-center text-sm">
+          <h2 class="text-2xl font-semibold mb-4">3. Utilisation de la Plateforme</h2>
+          <h3 class="text-xl font-semibold mb-2">3.1. Gestion de Mind Maps</h3>
+          <p>Les Utilisateurs peuvent créer, modifier et supprimer des mind maps. La collaboration en temps réel est possible, permettant à plusieurs Utilisateurs de travailler simultanément sur un même mind map.</p>
+          
+          <h3 class="text-xl font-semibold mt-4 mb-2">3.2. Gestion de Projets</h3>
+          <p>Les Utilisateurs peuvent créer des projets, ajouter des tâches, et assigner ces tâches à des membres. Un calendrier intégré permet de visualiser les échéances des projets et des tâches.</p>
+          
+          <h3 class="text-xl font-semibold mt-4 mb-2">3.3. Communication</h3>
+          <p>La Plateforme offre une messagerie instantanée et des forums de discussion pour faciliter la communication entre les Utilisateurs.</p>
+        </section>
+
+        <section class="text-center text-sm">
+          <h2 class="text-2xl font-semibold mb-4">4. Droits et Obligations des Utilisateurs</h2>
+          <h3 class="text-xl font-semibold mb-2">4.1. Respect des Droits d'Auteur</h3>
+          <p>Les Utilisateurs s'engagent à respecter les droits d'auteur et les droits de propriété intellectuelle des contenus publiés sur la Plateforme.</p>
+          
+          <h3 class="text-xl font-semibold mt-4 mb-2">4.2. Contenu Interdit</h3>
+          <p>Les Utilisateurs s'engagent à ne pas publier de contenu illégal, diffamatoire, obscène, ou portant atteinte à la vie privée ou aux droits de tiers.</p>
+          
+          <h3 class="text-xl font-semibold mt-4 mb-2">4.3. Utilisation Responsable</h3>
+          <p>Les Utilisateurs s'engagent à utiliser la Plateforme de manière responsable et conforme aux lois et règlements en vigueur.</p>
+        </section>
+
+        <section class="text-center text-sm">
+          <h2 class="text-2xl font-semibold mb-4">5. Protection des Données Personnelles</h2>
+          <h3 class="text-xl font-semibold mb-2">5.1. Collecte des Données</h3>
+          <p>La Plateforme collecte des Données Personnelles nécessaires à la création et à la gestion des Comptes. Les Utilisateurs sont informés de la collecte, de l'utilisation et du stockage de leurs Données Personnelles.</p>
+          
+          <h3 class="text-xl font-semibold mt-4 mb-2">5.2. Consentement</h3>
+          <p>L'Utilisateur donne son consentement explicite pour le traitement de ses Données Personnelles.</p>
+          
+          <h3 class="text-xl font-semibold mt-4 mb-2">5.3. Droits des Utilisateurs</h3>
+          <p>Les Utilisateurs disposent de droits d'accès, de rectification et de suppression de leurs Données Personnelles. Ils peuvent exercer ces droits en contactant l'administrateur de la Plateforme.</p>
+        </section>
+
+        <section class="text-center text-sm">
+          <h2 class="text-2xl font-semibold mb-4">6. Sécurité des Données</h2>
+          <p>La Plateforme met en place des mesures de sécurité appropriées pour protéger les Données Personnelles contre les accès non autorisés, la perte ou la divulgation.</p>
+        </section>
+
+        <section class="text-center text-sm">
+          <h2 class="text-2xl font-semibold mb-4">7. Responsabilité</h2>
+          <p>La Plateforme ne peut être tenue responsable des contenus publiés par les Utilisateurs. Les Utilisateurs sont seuls responsables des contenus qu'ils publient et des conséquences de leur publication.</p>
+        </section>
+
+        <section class="text-center text-sm">
+          <h2 class="text-2xl font-semibold mb-4">8. Modification des CGU</h2>
+          <p>La Plateforme se réserve le droit de modifier les présentes CGU à tout moment. Les modifications seront notifiées aux Utilisateurs par email ou par une annonce sur la Plateforme.</p>
+        </section>
+
+        <section class="text-center text-sm">
+          <h2 class="text-2xl font-semibold mb-4">9. Résiliation</h2>
+          <p>La Plateforme se réserve le droit de résilier le Compte d'un Utilisateur en cas de non-respect des présentes CGU.</p>
+        </section>
+
+        <section class="text-center text-sm">
+          <h2 class="text-2xl font-semibold mb-4">10. Loi Applicable et Juridiction</h2>
+          <p>Les présentes CGU sont régies par le droit français. Tout litige relatif à l'interprétation ou à l'exécution des présentes CGU sera soumis à la compétence exclusive des tribunaux français.</p>
+        </section>
+
+        <section class="text-center text-sm">
+          <h2 class="text-2xl font-semibold mb-4">11. Contact</h2>
+          <p>Pour toute question ou demande d'information concernant les présentes CGU, les Utilisateurs peuvent contacter l'administrateur de la Plateforme à l'adresse suivante : contact@tachbleu.fr.</p>
+        </section>
+
+        <section class="text-center text-sm">
+          <h2 class="text-xl font-semibold mb-2">En utilisant la Plateforme, vous acceptez les présentes CGU.</h2>
+          <p>Si vous n'acceptez pas ces conditions, veuillez ne pas utiliser la Plateforme.</p>
+        </section>
+
+        <section class="text-center text-sm">
+          <h2 class="text-xl font-semibold mb-2">Date de dernière mise à jour</h2>
+          <p>Les présentes CGU ont été mises à jour pour la dernière fois le 30/07/2024.</p>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style>
+body {
+  font-family: 'Inter', sans-serif;
+}
+</style>
