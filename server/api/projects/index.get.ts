0abbf580fa4b289b49a3ea6023b25a8934fbcd5b
Oussama Fatnassi <oussamafatnassi@icloud.com>
2024-07-30 09:40:24 +0200

    fixed the issue with projects not retrieving correctly

--- server/api/projects/index.get.ts
+++ server/api/projects/index.get.ts
@@ -1,18 +1,28 @@
 import { Project } from '@/server/models/project.model';
+import type { JwtPayload } from 'jsonwebtoken';
 import { User } from '~/server/models/user.model';
-
-// get sur projects si non admin ne renvoie que les projets sur lequel l'utilisateurs est membre
+import { verifyToken } from '~/server/utils/session';
 
 export default defineEventHandler(async (event) => {
     try {
         const projects = await Project.find();
-        const user = await User.findById(event.context.userId);
-        if (!user) throw createError({ statusCode: 401 });
-        if (user.role !== 'admin') return {
-            projects: projects.filter(project => user.projects?.includes(project._id) ?? false)
+        const token = getCookie(event, 'token');
+        if (!token) {
+            throw createError({
+                statusCode: 401,
+                statusMessage: 'Unauthorized'
+            })
         }
+        const user = await verifyToken(token) as JwtPayload;
+        if (!user) throw createError({ statusCode: 401 });
+        if (user.role === 'admin') {
+            return {
+                projects
+            }
+        }
+        const fullUser = await User.findById(user.id);
         return {
-            projects
+            projects: projects.filter(project => fullUser.projects?.includes(project._id) ?? false)
         }
     } catch {
         throw createError({
