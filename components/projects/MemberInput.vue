9d5c39142947fd1584eff1b25cbe1fe71903e65e
VIAUD Nathan <nathan.viaud@inria.fr>
2024-07-30 16:55:43 +0200

    improvement: add search

--- components/projects/MemberInput.vue
+++ components/projects/MemberInput.vue
@@ -20,6 +20,12 @@ function handleChange(option: UserWithoutPassword) {
     else emit('removeUser', option._id);
 }
 
+const search = ref('');
+
+const filteredOptions = computed(() => {
+    return props.options.filter((option) => option.name.toLowerCase().includes(search.value.toLowerCase())).slice(0, 10);
+});
+
 </script>
 
 <template>
@@ -31,16 +37,21 @@ function handleChange(option: UserWithoutPassword) {
             </Button>
         </PopoverTrigger>
 
-        <PopoverContent class="w-[200px] p-2 flex flex-col gap-2" align="start">
-            <p class="text-sm font-semibold">Users</p>
-            <div v-for="option of options" class="flex items-center gap-2">
-                <Checkbox
-                    :id="option._id"
-                    :default-checked="selected.includes(option._id)"
-                    :disabled="option.role === 'admin'"
-                    @update:checked="handleChange(option)"
-                />
-                <label class="text-sm select-none cursor-pointer" :for="option._id">{{ option.name }}</label>
+        <PopoverContent class="w-[200px] p-0 flex flex-col gap-2" align="start">
+            <input v-model="search" class="p-2 w-full bg-transparent border-0 outline-0 placeholder-muted-foreground" type="text" placeholder="Search..." />
+            <Separator orientation="horizontal" />
+            <div class="p-2">
+                <p class="text-sm font-semibold">Users</p>
+                <div v-for="option of filteredOptions" class="flex items-center gap-2">
+                    <Checkbox
+                        :id="option._id"
+                        :default-checked="selected.includes(option._id)"
+                        :disabled="option.role === 'admin'"
+                        @update:checked="handleChange(option)"
+                    />
+                    <label class="text-sm select-none cursor-pointer" :for="option._id">{{ option.name }}</label>
+                </div>
+                <p v-if="filteredOptions.length === 0" class="text-sm italic text-muted-foreground">No users found</p>
             </div>
         </PopoverContent>
     </Popover>
