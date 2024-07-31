6b0af9a6dc25748a74c3f932fb23ac96a0a7e192
VIAUD Nathan <nathan.viaud@inria.fr>
2024-07-31 08:32:48 +0200

    fix: alert dialog

--- app.vue
+++ app.vue
@@ -3,22 +3,23 @@
       <Header />
       <NuxtPage class="flex-grow p-4"/>
       <ClientOnly>
-        <Dialog :open="showCookieConsent && route.path !== '/conditions'" @update:open="showCookieConsent = $event">
-          <DialogContent class="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Consentement des cookies</DialogTitle>
-              <DialogDescription>
-                Nous utilisons des cookies pour améliorer votre expérience. Pour continuer à utiliser ce site, veuillez accepter notre utilisation de cookies.
-              </DialogDescription>
-            </DialogHeader>
-            <div class="flex justify-between mt-4">
-              <Button @click="acceptCookies">Accepter</Button>
-              <NuxtLink to="/conditions">
-                <Button variant="outline">Plus d'informations</Button>
-              </NuxtLink>
-            </div>
-          </DialogContent>
-        </Dialog>
+          <AlertDialog :open="showCookieConsent && route.path !== '/conditions'" @update:open="showCookieConsent = $event">
+              <AlertDialogContent>
+                  <AlertDialogHeader>
+                      <AlertDialogTitle>Consentement des cookies</AlertDialogTitle>
+                      <AlertDialogDescription>
+                          Nous utilisons des cookies pour améliorer votre expérience. Pour continuer à utiliser ce site, veuillez accepter notre utilisation de cookies.
+                      </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                      <AlertDialogAction @click="acceptCookies">Accepter</AlertDialogAction>
+                      <!--                  <Button @click="acceptCookies">Accepter</Button>-->
+                      <NuxtLink to="/conditions">
+                          <Button variant="outline">Plus d'informations</Button>
+                      </NuxtLink>
+                  </AlertDialogFooter>
+              </AlertDialogContent>
+          </AlertDialog>
       </ClientOnly>
     </div>
   </template>
@@ -26,8 +27,8 @@
   <script setup lang="ts">
   import { useRoute } from 'vue-router'
   import { useCookieConsent } from '~/composables/useCookieConsent'
-  import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '~/components/ui/dialog'
   import { Button } from '~/components/ui/button'
+  import { AlertDialog, AlertDialogContent, AlertDialogHeader, AlertDialogTitle, AlertDialogAction, AlertDialogFooter } from '~/components/ui/alert-dialog';
   
   const route = useRoute()
   const { showCookieConsent, acceptCookies } = useCookieConsent()
